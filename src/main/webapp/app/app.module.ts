3f4af87337bfd7bdc7a2047fd4532f54273ff3ce
jdubois <jdubois@jhipster-app.c.jhipster-online.internal>
2017-08-24 13:15:12 +0000

    Initial application generation by JHipster

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,57 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { JhipsterSampleApplication4SharedModule, UserRouteAccessService } from './shared';
+import { JhipsterSampleApplication4HomeModule } from './home/home.module';
+import { JhipsterSampleApplication4AdminModule } from './admin/admin.module';
+import { JhipsterSampleApplication4AccountModule } from './account/account.module';
+import { JhipsterSampleApplication4EntityModule } from './entities/entity.module';
+
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+// jhipster-needle-angular-add-module-import JHipster will add new module here
+
+import {
+    JhiMainComponent,
+    LayoutRoutingModule,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    ErrorComponent
+} from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        JhipsterSampleApplication4SharedModule,
+        JhipsterSampleApplication4HomeModule,
+        JhipsterSampleApplication4AdminModule,
+        JhipsterSampleApplication4AccountModule,
+        JhipsterSampleApplication4EntityModule,
+        // jhipster-needle-angular-add-module JHipster will add new module here
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        ActiveMenuDirective,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class JhipsterSampleApplication4AppModule {}
